487bc5c002c13f94707dc36c12f49a63a194ccff
Leonardo Vasques <quenixserra@gmail.com>
2022-07-29 01:39:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 
-async function download(){
-    let videoID = 'https://www.youtube.com/watch?v=NdYWuo9OFAw&list=PLj1EqwPS3ZA5Con3CjgzOIMZyeNiNsnsi'
+async function download(videoID){
     let info = await ytdl.getInfo(videoID);
-    let format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-    console.log('Format found!', /*  */info.videoDetails.title);
+    console.log('Started downloading...', /*  */info.videoDetails.title);
     
     ytdl(videoID, {
         quality: `highest`
       })
     .pipe(fs.createWriteStream(`${info.videoDetails.title}.mp4`));
+
+    console.log("Download complete!")
 }
 
-download()
-
-if (true) {
-    return
-}
+download('https://www.youtube.com/watch?v=djV11Xbc914&list=PLj1EqwPS3ZA5Con3CjgzOIMZyeNiNsnsi&index=10')
+download("https://www.youtube.com/watch?v=-cJauX_q6wI&list=PLj1EqwPS3ZA5Con3CjgzOIMZyeNiNsnsi&index=13")
 
